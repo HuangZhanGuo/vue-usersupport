d9104aab24d91e6e0e76d853e34178f16f1b8da9
yaozun <2812575654@qq.com>
2018-05-29 10:04:14 +0800

    权限-角色管理

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ import baseInfomation from '../components/systemConfig/base_infomation'
 import resetPassword from '../components/systemConfig/reset_password'
 import userManager from '../components/permissionManagerment/userManager'
 import menuManager from '../components/permissionManagerment/menuManager'
+import roleManager from '../components/permissionManagerment/roleManager'
 
 Vue.use(Router)
 
@@ -63,6 +64,10 @@ const router = new Router({
         {
           path: '/pm/menuManager',
           component: menuManager
+        },
+        {
+          path: '/pm/roleManager',
+          component: roleManager
         },
         {
          path: '/um/addUser',
